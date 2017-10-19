1c563c70ebfbccfcf55e98dc3d07626ed0713f4d
Zhaowei Yu <yougwich@163.com>
2017-10-19 14:42:49 -0400

    1019/2

--- map.js
+++ map.js
@@ -17,78 +17,202 @@ var map = new mapboxgl.Map({
 	style: 'mapbox://styles/13777726133/cj8n4fi6e7ev32soro0kewsky' 
 });
 
-// FAILED!!! Create a custom layer switcher to display different datasets
-// map.on('load', function () {
-//     map.addSource('museums', {
-//         type: 'vector',
-//         url: 'mapbox://mapbox.2opop9hr'
-//     });
-//     map.addLayer({
-//         'id': 'museums',
-//         'type': 'circle',
-//         'source': 'museums',
-//         'layout': {
-//             'visibility': 'visible'
-//         },
-//         'paint': {
-//             'circle-radius': 8,
-//             'circle-color': 'rgba(55,148,179,1)'
-//         },
-//         'source-layer': 'museum-cusco'
+
+//Create a custom layer switcher to display different datasets
+map.on('load', function () {
+    map.addSource('startpoint', {
+        type: 'vector',
+        url: 'mapbox://13777726133.aruiio16'
+    });
+    map.addLayer({
+        'id': 'startpoint',
+        'type': 'circle',
+        'source': 'startpoint',
+        'layout': {
+            'visibility': 'visible'
+        },
+        'paint': {
+            'circle-radius': 5,
+            'circle-color': 'rgba(219,36,60,0.2)'
+        },
+        'source-layer': '20170901-Filtered-Morning-Rus-559sek'
+    });
+
+    map.addSource('endpoint', {
+        type: 'vector',
+        url: 'mapbox://13777726133.aruiio16'
+    });
+    map.addLayer({
+        'id': 'endpoint',
+        'type': 'circle',
+        'source': 'endpoint',
+        'layout': {
+            'visibility': 'visible'
+        },
+        'paint': {
+            'circle-radius': 5,
+            'circle-color': 'rgba(143,227,135,0.11)'
+        },
+        'source-layer': '20170901-Filtered-Morning-Rus-559sek'
+    });
+
+    // map.addSource('contours', {
+    //     type: 'vector',
+    //     url: 'mapbox://mapbox.mapbox-terrain-v2'
+    // });
+    // map.addLayer({
+    //     'id': 'contours',
+    //     'type': 'line',
+    //     'source': 'contours',
+    //     'source-layer': 'contour',
+    //     'layout': {
+    //         'visibility': 'visible',
+    //         'line-join': 'round',
+    //         'line-cap': 'round'
+    //     },
+    //     'paint': {
+    //         'line-color': '#877b59',
+    //         'line-width': 1
+    //     }
+    // });
+});
+
+var toggleableLayerIds = [ 'startpoint', 'endpoint' ];
+
+for (var i = 0; i < toggleableLayerIds.length; i++) {
+    var id = toggleableLayerIds[i];
+
+    var link = document.createElement('a');
+    link.href = '#';
+    link.className = 'active';
+    link.textContent = id;
+
+    link.onclick = function (e) {
+        var clickedLayer = this.textContent;
+        e.preventDefault();
+        e.stopPropagation();
+
+        var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
+
+        if (visibility === 'visible') {
+            map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+            this.className = '';
+        } else {
+            this.className = 'active';
+            map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+        }
+    };
+
+    var layers = document.getElementById('menu');
+    layers.appendChild(link);
+}
+
+
+// // Create a heat map layer
+// map.on('load', function() {
+//     //Add a geojson point source.  
+//     //Heatmap layers also work with a vector tile source.
+//     map.addSource('earthquakes', {
+//         "type": "geojson",
+//         "data": "https://www.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson"
 //     });
 
-//     map.addSource('contours', {
-//         type: 'vector',
-//         url: 'mapbox://mapbox.mapbox-terrain-v2'
-//     });
 //     map.addLayer({
-//         'id': 'contours',
-//         'type': 'line',
-//         'source': 'contours',
-//         'source-layer': 'contour',
-//         'layout': {
-//             'visibility': 'visible',
-//             'line-join': 'round',
-//             'line-cap': 'round'
-//         },
-//         'paint': {
-//             'line-color': '#877b59',
-//             'line-width': 1
+//         "id": "earthquakes-heat",
+//         "type": "heatmap",
+//         "source": "earthquakes",
+//         "maxzoom": 9,
+//         "paint": {
+//             //Increase the heatmap weight based on frequency and property magnitude
+//             "heatmap-weight": {
+//                 "property": "mag",
+//                 "type": "exponential",
+//                 "stops": [
+//                     [0, 0],
+//                     [6, 1]
+//                 ]
+//             },
+//             //Increase the heatmap color weight weight by zoom level
+//             //heatmap-ntensity is a multiplier on top of heatmap-weight
+//             "heatmap-intensity": {
+//                 "stops": [
+//                     [0, 1],
+//                     [9, 3]
+//                 ]
+//             },
+//             //Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+//             //Begin color ramp at 0-stop with a 0-transparancy color
+//             //to create a blur-like effect.
+//             "heatmap-color": {
+//                 "stops": [
+//                     [0, "rgba(33,102,172,0)"],
+//                     [0.2, "rgb(103,169,207)"],
+//                     [0.4, "rgb(209,229,240)"],
+//                     [0.6, "rgb(253,219,199)"],
+//                     [0.8, "rgb(239,138,98)"],
+//                     [1, "rgb(178,24,43)"]
+//                 ]
+//             },
+//             //Adjust the heatmap radius by zoom level
+//             "heatmap-radius": {
+//                 "stops": [
+//                     [0, 2],
+//                     [9, 20]
+//                 ]
+//             },
+//             //Transition from heatmap to circle layer by zoom level
+//             "heatmap-opacity": {
+//                 "default": 1,
+//                 "stops": [
+//                     [7, 1],
+//                     [9, 0]
+//                 ]
+//             },
 //         }
-//     });
+//     }, 'waterway-label');
+
+//     map.addLayer({
+//         "id": "earthquakes-point",
+//         "type": "circle",
+//         "source": "earthquakes",
+//         "minzoom": 7,
+//         "paint": {
+//             //Size circle raidus by earthquake magnitude and zoom level
+//             "circle-radius": {
+//                 "property": "mag",
+//                 "type": "exponential",
+//                 "stops": [
+//                     [{ zoom: 7, value: 1 }, 1],
+//                     [{ zoom: 7, value: 6 }, 4],
+//                     [{ zoom: 16, value: 1 }, 5],
+//                     [{ zoom: 16, value: 6 }, 50],
+//                 ]
+//             },
+//             //Color circle by earthquake magnitude
+//             "circle-color": {
+//                 "property": "mag",
+//                 "type": "exponential",
+//                 "stops": [
+//                     [1, "rgba(33,102,172,0)"],
+//                     [2, "rgb(103,169,207)"],
+//                     [3, "rgb(209,229,240)"],
+//                     [4, "rgb(253,219,199)"],
+//                     [5, "rgb(239,138,98)"],
+//                     [6, "rgb(178,24,43)"]
+//                 ]
+//             },
+//             "circle-stroke-color": "white",
+//             "circle-stroke-width": 1,
+//             //Transition from heatmap to circle layer by zoom level
+//             "circle-opacity": {
+//                 "stops": [
+//                     [7, 0],
+//                     [8, 1]
+//                 ]
+//             }
+//         }
+//     }, 'waterway-label');
 // });
-
-// var toggleableLayerIds = [ 'contours', 'museums' ];
-
-// for (var i = 0; i < toggleableLayerIds.length; i++) {
-//     var id = toggleableLayerIds[i];
-
-//     var link = document.createElement('a');
-//     link.href = '#';
-//     link.className = 'active';
-//     link.textContent = id;
-
-//     link.onclick = function (e) {
-//         var clickedLayer = this.textContent;
-//         e.preventDefault();
-//         e.stopPropagation();
-
-//         var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
-
-//         if (visibility === 'visible') {
-//             map.setLayoutProperty(clickedLayer, 'visibility', 'none');
-//             this.className = '';
-//         } else {
-//             this.className = 'active';
-//             map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
-//         }
-//     };
-
-//     var layers = document.getElementById('menu');
-//     layers.appendChild(link);
-// }
-
-
 
 
 
@@ -139,11 +263,11 @@ map.on('load', function() {	// the event listener that does some code after the 
 	map.on('mousemove', function(e) {	// event listener to do some code when the mouse moves
 
 	  var lanes = map.queryRenderedFeatures(e.point, {
-	    layers: ['nyc-bike-route']	// replace 'cville-parks' with the name of your layer, if using a different layer
+	    layers: ['nyc-bike-routes-1-bike-lane', 'nyc-bike-routes-2-bike-lane', 'nyc-bike-routes-3-bike-lane copy', 'nyc-bike-routes-4-bike-lane copy 1']	// replace 'cville-parks' with the name of your layer, if using a different layer
 	  });
 
 	  if (lanes.length > 0) {	// if statement to make sure the following code is only added to the info window if the mouse moves over a state
-	    document.getElementById('info-window-body').innerHTML = '<h3><strong>' + lanes[0].properties.lanecount + 1 + 'bike lane each direction</strong></h3>';
+	    document.getElementById('info-window-body').innerHTML = '<h3><strong>' + parseInt(lanes[0].properties.lanecount + 1) + ' bike lane each direction</strong></h3>';
 	  } else {
 	    document.getElementById('info-window-body').innerHTML = '<p>Hover over a bike lane or click on a bike station to learn more about it.</p>';
 	  }
@@ -151,6 +275,7 @@ map.on('load', function() {	// the event listener that does some code after the 
 	});
 
 
+// 		Hao's code for toggle layers
 
 // function openMenu(evt, menuName) {
 //     var i, tabcontent, tablinks;
